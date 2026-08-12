37d1e419795b16bd4ece8edd5554afee8332b94b
Enis Şahin <enissahin04@gmail.com>
2026-08-12 21:29:04 +0300

    Add sudoku rule enforcement

--- sudoku.c
+++ sudoku.c
@@ -1,5 +1,39 @@
 #include <stdio.h>
 
+int satirdavarmi(int matris[][9], int satir,int deger){
+    int j;
+    for(j=0; j<9; j++){
+        if(matris[satir][j]==deger){
+            return 1;
+        }
+    }
+return 0;
+}
+int sutundavarmi(int matris[][9],int sutun ,int deger){
+    int i;
+    for(i=0; i<9; i++){
+        if(matris[i][sutun]==deger){
+            return 1;
+        }
+    }
+    return 0;
+}
+
+ int kutudavarmi(int matris[][9],int satir,int sutun,int deger){
+int i,j,kutusatiri,kutusutunu;
+
+kutusatiri=(satir/3)*3;
+kutusutunu=(sutun/3)*3;
+    for(i=0; i<3; i++){
+        for(j=0; j<3; j++){
+            if(matris[i+kutusatiri][j+kutusutunu]==deger){
+                return 1;
+            }
+        }
+    }
+    return 0;
+ }
+
 int main()
 {
     printf("Sudoku projesi basliyor!\n");
@@ -38,7 +72,21 @@ int main()
     scanf("%d",&sutun);
     printf("girdiğiniz konumdaki değeri değiştiniz :");
     scanf("%d",&deger);
-    matris[satir-1][sutun-1]=deger;
+    if(satirdavarmi(matris,satir-1,deger)){
+        printf("satırda bu sayı zaten var. tekrar deneyiniz.\n");
+        continue;
+    }
+    if(sutundavarmi(matris,sutun-1,deger)){
+        printf("bu sayı sütunda zaten var. tekrar deneyiniz.\n");
+        continue;
+    }
+    if(kutudavarmi(matris,satir-1,sutun-1,deger)){
+        printf("3x3 kutuda zaten bu sayı var.\n");
+        continue;
+    }
+    else{
+        matris[satir-1][sutun-1]=deger;
+    }
    }
 
 printf("oluşturduğunuz matris şu şekildedir :\n");
